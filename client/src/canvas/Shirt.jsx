2dbdadba48c331e936fbe597b009f1492d401457
JP <jay.patel.py@gmail.com>
2023-06-04 22:36:49 -0600

    fixed typos

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import { easing } from 'maath';
 import { useSnapshot } from 'valtio';
 import { useFrame } from '@react-three/fiber';
@@ -13,7 +13,7 @@ const Shirt = () => {
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
 
-  useFrame((state, delta) => easing.dampC(materials.lambert.color, snap.color, 0.25, delta));
+  useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta));
 
   const stateString = JSON.stringify(snap);
 
@@ -22,29 +22,30 @@ const Shirt = () => {
       <mesh
         castShadow
         geometry={nodes.T_Shirt_male.geometry}
-        materials={materials.lambert}
+        material={materials.lambert1}
         material-roughness={1}
         dispose={null}
       >
-      {snap.isFullTexture && (
-        <Decal
-          position={[0, 0, 0]}
-          rotation={[0, 0, 0]}
-          scale={1}
-          map={fullTexture}
-        />
-      )}
-      {snap.isLogoTexture && (
-        <Decal
-          position={[0, 0.04, 15]}
-          rotation={[0, 0, 0]}
-          scale={0.15}
-          map={logoTexture}
-          map-anisotropy={16}
-          depthTest={false}
-          depthWrite={true}
-        />
-      )}
+        {snap.isFullTexture && (
+          <Decal 
+            position={[0, 0, 0]}
+            rotation={[0, 0, 0]}
+            scale={1}
+            map={fullTexture}
+          />
+        )}
+
+        {snap.isLogoTexture && (
+          <Decal 
+            position={[0, 0.04, 0.15]}
+            rotation={[0, 0, 0]}
+            scale={0.15}
+            map={logoTexture}
+            map-anisotropy={16}
+            depthTest={false}
+            depthWrite={true}
+          />
+        )}
       </mesh>
     </group>
   )
