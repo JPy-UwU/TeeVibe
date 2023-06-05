a9061ab26a5d522bfdad39be9f6da0085f6d924d
JP <jay.patel.py@gmail.com>
2023-06-04 21:36:14 -0600

    Created and imported canvas components

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,6 +1,10 @@
 import { Canvas }  from '@react-three/fiber';
 import { Environment , Center } from '@react-three/drei';
 
+import Shirt from './Shirt';
+import BackDrop from './BackDrop';
+import CameraRig from './CameraRig';
+
 const CanvasModel = () => {
   return (
     <div>Canvas</div>
