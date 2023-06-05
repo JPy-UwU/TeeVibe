eefa1a32652d8d9a5345c7371194144f49c86570
JP <jay.patel.py@gmail.com>
2023-06-04 21:14:56 -0600

    Added and updated button

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -8,7 +8,7 @@ import { download } from '../assets';
 import { downloadCanvasToImage, reader } from '../config/helpers';
 import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
 import { fadeAnimation , slideAnimation } from '../config/motion';
-import { AIPicker, ColorPicker, FilePicker, Tab } from '../components'
+import { AIPicker, ColorPicker, FilePicker, Tab, CustomButton } from '../components'
 
 
 const Customizer = () => {
@@ -35,6 +35,19 @@ const Customizer = () => {
             </div>
           </div>
         </motion.div>
+
+        <motion.div
+          className='absolute z-10 top-5 right-5'
+          {...fadeAnimation}
+        >
+          <CustomButton 
+            type="filled"
+            title="Go Back"
+            handleClick={() => state.intro = true}
+            customStyle="w-fit px-4 py-2.5 font-bold text-sm"
+          />
+        </motion.div>
+
        </>
       )}
     </AnimatePresence>
