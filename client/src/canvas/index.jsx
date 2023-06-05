7bab4866b491585f3540e990d87839098cff0cc1
JP <jay.patel.py@gmail.com>
2023-06-04 21:43:53 -0600

    Added shirt model

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -7,7 +7,18 @@ import CameraRig from './CameraRig';
 
 const CanvasModel = () => {
   return (
-    <div>Canvas</div>
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset='city' />
+
+      {/* <CameraRig> */}
+        {/* <BackDrop /> */}
+        <Center>
+          <Shirt />
+        </Center>
+      {/* </CameraRig> */}
+
+    </Canvas>
   )
 }
 
